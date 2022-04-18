46f7bca779f23ccf2739a64e91d180f4ca877424
Menna13 <mkhaliel@Mennas-MacBook-Pro.local>
2022-04-18 05:19:31 -0400

    initial app

---
+++ VolunteerApp/app/src/main/java/edu/brynmawr/volunteerapp/Event.java
@@ -0,0 +1,18 @@
+package edu.brynmawr.volunteerapp;
+
+public class Event {
+//    public String name;
+//    public String screenName;
+//    public String profileImageUrl;
+//
+//    //empty constructor needed by the parceler library
+//    public User(){}
+//
+//    public static User fromJson(JSONObject jsonObject) throws JSONException {
+//        User user = new User();
+//        user.name = jsonObject.getString("name");
+//        user.screenName = jsonObject.getString("screen_name");
+//        user.profileImageUrl = jsonObject.getString("profile_image_url_https");
+//        return user;
+//    }
+}
---
+++ VolunteerApp/app/src/main/java/edu/brynmawr/volunteerapp/ViewEvents.java
@@ -0,0 +1,14 @@
+package edu.brynmawr.volunteerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_events);
+    }
+}
