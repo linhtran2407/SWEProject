2970f3658203a692d66392cefd16fc3f3fd95366
Menna13 <mkhaliel@wc120-dhcp-141.brynmawr.edu>
2022-04-22 14:47:44 -0400

    new layouts

--- VolunteerApp/app/src/main/java/edu/brynmawr/volunteerapp/CreateEvent.java
+++ VolunteerApp/app/src/main/java/edu/brynmawr/volunteerapp/CreateEvent.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.json.JSONObject;
@@ -15,66 +18,124 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class CreateEvent extends AppCompatActivity {
+public class CreateEvent
+        extends AppCompatActivity
+        implements AdapterView.OnItemSelectedListener {
+
+    // create array of Strings
+    // and store name of courses
+    String[] courses = { "C", "Data structures",
+            "Interview prep", "Algorithms",
+            "DSA with java", "OS" };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_event);
+        setContentView(R.layout.activity_main);
+
+        // Take the instance of Spinner and
+        // apply OnItemSelectedListener on it which
+        // tells which item of spinner is clicked
+        Spinner spino = findViewById(R.id.categoryspinner);
+        spin.setOnItemSelectedListener(this);
+
+        // Create the instance of ArrayAdapter
+        // having the list of courses
+        ArrayAdapter ad
+                = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                courses);
+
+        // set simple layout resource file
+        // for each item of spinner
+        ad.setDropDownViewResource(
+                android.R.layout
+                        .simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter (ad) data on the
+        // Spinner which binds data to spinner
+        spino.setAdapter(ad);
     }
 
-    protected String message;
+    // Performing action when ItemSelected
+    // from spinner, Overriding onItemSelected method
+    @Override
+    public void onItemSelected(AdapterView<*> arg0,
+                               View arg1,
+                               int position,
+                               long id)
+    {
 
-
-    public void onConnectButtonClick(View v) {
-
-        TextView tv = findViewById(R.id.statusField);
-
-        try {
-            ExecutorService executor = Executors.newSingleThreadExecutor();
-            executor.execute( () -> {
-                        try {
-                            // assumes that there is a server running on the AVD's host on port 3000
-                            // and that it has a /test endpoint that returns a JSON object with
-                            // a field called "message"
-
-                            URL url = new URL("http://10.0.2.2:3000/create");
-
-                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                            conn.setRequestMethod("POST");
-                            conn.connect();
-
-                            Scanner in = new Scanner(url.openStream());
-                            String response = in.nextLine();
-
-                            JSONObject jo = new JSONObject(response);
-
-                            // need to set the instance variable in the Activity object
-                            // because we cannot directly access the TextView from here
-                            message = jo.getString("message");
-
-                        }
-                        catch (Exception e) {
-                            e.printStackTrace();
-                            message = e.toString();
-                        }
-                    }
-            );
-
-            // this waits for up to 2 seconds
-            // it's a bit of a hack because it's not truly asynchronous
-            // but it should be okay for our purposes (and is a lot easier)
-            executor.awaitTermination(2, TimeUnit.SECONDS);
-
-            // now we can set the status in the TextView
-            tv.setText(message);
-        }
-        catch (Exception e) {
-            // uh oh
-            e.printStackTrace();
-            tv.setText(e.toString());
-        }
-
-
+        // make toastof name of course
+        // which is selected in spinner
+        Toast.makeText(getApplicationContext(),
+                courses[position],
+                Toast.LENGTH_LONG)
+                .show();
     }
+
+    @Override
+    public void onNothingSelected(AdapterView<*> arg0)
+    {
+        // Auto-generated method stub
+    }
+}
+
+
+//    protected String message;
+//
+//
+//    public void onConnectButtonClick(View v) {
+//
+//        TextView tv = findViewById(R.id.statusField);
+//
+//        try {
+//            ExecutorService executor = Executors.newSingleThreadExecutor();
+//            executor.execute( () -> {
+//                        try {
+//                            // assumes that there is a server running on the AVD's host on port 3000
+//                            // and that it has a /test endpoint that returns a JSON object with
+//                            // a field called "message"
+//
+//                            URL url = new URL("http://10.0.2.2:3000/create");
+//
+//                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                            conn.setRequestMethod("POST");
+//                            conn.connect();
+//
+//                            Scanner in = new Scanner(url.openStream());
+//                            String response = in.nextLine();
+//
+//                            JSONObject jo = new JSONObject(response);
+//
+//                            // need to set the instance variable in the Activity object
+//                            // because we cannot directly access the TextView from here
+//                            message = jo.getString("message");
+//
+//                        }
+//                        catch (Exception e) {
+//                            e.printStackTrace();
+//                            message = e.toString();
+//                        }
+//                    }
+//            );
+//
+//            // this waits for up to 2 seconds
+//            // it's a bit of a hack because it's not truly asynchronous
+//            // but it should be okay for our purposes (and is a lot easier)
+//            executor.awaitTermination(2, TimeUnit.SECONDS);
+//
+//            // now we can set the status in the TextView
+//            tv.setText(message);
+//        }
+//        catch (Exception e) {
+//            // uh oh
+//            e.printStackTrace();
+//            tv.setText(e.toString());
+//        }
+//
+//
+//    }
 }
--- VolunteerApp/app/src/main/java/edu/brynmawr/volunteerapp/MainActivity.java
+++ VolunteerApp/app/src/main/java/edu/brynmawr/volunteerapp/MainActivity.java
@@ -2,8 +2,10 @@ package edu.brynmawr.volunteerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.json.JSONObject;
@@ -16,12 +18,31 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnAddEvent;
+    Button btnViewEvents;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAddEvent = findViewById(R.id.btnAddEvent);
+        btnViewEvents = findViewById(R.id.btnViewEvents);
+        btnAddEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createEvent = new Intent(MainActivity.this, CreateEvent.class);
+                startActivity(createEvent);
+            }
+        });
+        btnViewEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent viewEvents = new Intent(MainActivity.this, ViewEvents.class);
+                startActivity(viewEvents);
+            }
+        });
     }
+
+
 //    protected String message;
 //
 //
